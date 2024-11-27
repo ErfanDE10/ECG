50f470fcc3f466ca0453de891d2d600c89fe06fc
Erfan Zarabadi pour <150175418+ErfanDE10@users.noreply.github.com>
2024-11-27 23:30:49 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -16,4 +17,5 @@ def get_data():
     return jsonify({"heart_rate": 75, "oxygen_level": 98}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
